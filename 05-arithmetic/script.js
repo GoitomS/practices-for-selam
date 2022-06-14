75d11b219e0eb9ceffe762c64e719dc590ab0ddc
Goitom Kassaye <japu111@yahoo.com>
2022-06-13 23:25:25 -0700

    first commit

---
+++ 05-arithmetic/script.js
@@ -0,0 +1,22 @@
+// create names for your variables, they should perform mathematical operations on the following two variables
+let num1 = 3;
+let num2 = 9;
+
+// THIS SHOULD ADD
+ let add = num1 + num2;
+ 
+ console.log(add);
+
+// THIS SHOULD SUBTRACT NUM1 FROM NUM2 
+//  let  = ;
+
+// THIS SHOULD MULTIPLY NUM1 & NUM2 
+
+
+// THIS SHOULD DIVIDE NUM1 FROM NUM2 
+
+
+// THIS SHOULD FIND THE MODULUS OF NUM1 FROM NUM2 
+
+// print your answers in the console
+
